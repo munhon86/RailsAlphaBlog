119e284014d2dabd45db9684f0cf2874839d60e7
munhon86 <munhon_86@hotmail.com>
2020-03-06 13:26:27 +0000

    remove hardcoding of @article.user  value in articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
         #render plain: params[:article].inspect
         #debugger
         @article = Article.new(article_params)
-        @article.user = User.find(10)
+        @article.user = current_user
         if @article.save
             flash[:success] = "Article was successfully created!"
             redirect_to article_path(@article)
