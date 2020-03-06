3e08d2d60c1b4737829fd8e3ecc1bbb84df0d83a
munhon86 <munhon_86@hotmail.com>
2020-03-06 10:21:11 +0000

    restrict actions from UI

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,12 @@ class ApplicationController < ActionController::Base
     helper_method :current_user, :logged_in?
     
     def current_user
-        @user ||= User.find(session[:user_id]) if session[:user_id]
+        #debugger
+        @curr_user ||= User.find(session[:user_id]) if session[:user_id]
     end
     
     def logged_in?
-        !!@user
+        !!current_user
     end
     
     def require_user
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,7 @@ class SessionsController < ApplicationController
     end
     
     def destroy
+        
         session[:user_id] = nil
         flash[:success] = "You have logged out"
         redirect_to root_path
