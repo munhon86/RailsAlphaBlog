ee7abbc30e187e96f03dd614896d693269f0e490
munhon86 <munhon_86@hotmail.com>
2020-03-07 04:13:52 +0000

    add admin user functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -58,7 +58,7 @@ class ArticlesController < ApplicationController
         
         def require_same_user
             
-            if current_user != @article.user
+            if current_user != @article.user && !current_user.admin?
                 flash[:danger] = "You can only delete your own article"
                 redirect_to root_path
             end
